95d3b6ad0f9d7f7b27b8d37b2d20c5a2f49c1ef1
Muhammad-Mahir157 <hmmahir2001@gmail.com>
2024-03-10 21:38:18 +0500

    added the route for wild card route

--- kanbab-board-application/src/app/app.routes.ts
+++ kanbab-board-application/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { ManiViewComponent } from './modules/main-view/main-view.component';
 
 export const routes: Routes = [
-    {path: '', component: ManiViewComponent}
+    {path: '', component: ManiViewComponent},
+    { path: '**', component: ManiViewComponent}
 ];
